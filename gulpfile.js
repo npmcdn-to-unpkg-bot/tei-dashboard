0d3d1f735639b2bb7855b01287d01dcd724d433c
Kyle <kylerose@gmail.com>
2016-01-29 14:31:00 -0500

    added gulp utils

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,17 @@
 var gulp = require('gulp');
+var gutil = require('gulp-util');
 var sass = require('gulp-sass');
 
 gulp.task('styles', function() {
+    
     gulp.src('./assets/sass/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./assets/css/'));
+        return gutil.log('processing SCSS');
 });
 //Watch task
 gulp.task('default', function() {
+    
     gulp.watch('./assets/sass/*.scss', ['styles']);
+    return gutil.log('Gulp is watching!');
 });
